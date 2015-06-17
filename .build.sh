db20253eb9f9a214f2848ed60f254f06f5a1486d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-06-18 00:00:00 +0200

    Create a release archive.

--- .build.sh
+++ .build.sh
@@ -79,26 +79,26 @@ cd ..
 #
 # Assemble the artifacts.
 #
-#rm -rf build
-#mkdir build
-#cd build
-#cmake ../installer/ivy
-#STATUS=$?
-#if [ $STATUS -ne 0 ]; then
-#	exit 1
-#fi
-#
-#make
-#STATUS=$?
-#if [ $STATUS -ne 0 ]; then
-#	exit 1
-#fi
-#
-#make install
-#STATUS=$?
-#if [ $STATUS -ne 0 ]; then
-#	exit 1
-#fi
-#
-#cd ..
+rm -rf build
+mkdir build
+cd build
+cmake ../installer/ivy
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+make install
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+	exit 1
+fi
+
+cd ..
 
