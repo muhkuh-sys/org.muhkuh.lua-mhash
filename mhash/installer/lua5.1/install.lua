bd5c1320389faf201504ae672fbf95dad1ef594e
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-22 09:33:00 +0200

    Drop support for Ubuntu 16.10 .

--- mhash/installer/lua5.1/install.lua
+++ mhash/installer/lua5.1/install.lua
@@ -30,15 +30,6 @@ elseif strDistId=='ubuntu' then
       tResult = true
     end
 
-  elseif strDistVersion=='16.10' then
-    if strCpuArch=='x86' then
-      t:install('ubuntu_1610_x86/lua_plugins/mhash.so',       '${install_lua_cpath}/')
-      tResult = true
-    elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1610_x86_64/lua_plugins/mhash.so',    '${install_lua_cpath}/')
-      tResult = true
-    end
-
   elseif strDistVersion=='17.04' then
     if strCpuArch=='x86' then
       t:install('ubuntu_1704_x86/lua_plugins/mhash.so',       '${install_lua_cpath}/')
